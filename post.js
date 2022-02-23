479c1f0ab04ca42fb0f2a28227e1bc5c060d0177
NishatFariza <nishatfarzia78@gmail.com>
2022-02-23 22:04:18 +0600

    load post and display on the website with css

--- post.js
+++ post.js
@@ -9,7 +9,12 @@ function displayPost(posts){
     console.log(posts)
     const postContainer =document.getElementById('posts')
     for (const post of posts){
-        console.log(post.title)
+        const div =document.createElement('div')
+        div.classList.add('post');
+        div.innerHTML =`<h3>${post.title}</h3>
+           <p>${post.body}</p>
+        `
+        postContainer.appendChild(div)
     }
-
+   
 }
