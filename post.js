4ba613c1decc7c6bea9fdd37ba5578e986efbc7d
NishatFariza <nishatfarzia78@gmail.com>
2022-02-23 23:12:56 +0600

    post add

--- post.js
+++ post.js
@@ -10,11 +10,29 @@ function displayPost(posts){
     const postContainer =document.getElementById('posts')
     for (const post of posts){
         const div =document.createElement('div')
-        div.classList.add('post');
-        div.innerHTML =`<h3>${post.title}</h3>
+        div.classList.add('post');//css style add and
+        div.innerHTML =`<h3>${post.title}</h3> 
            <p>${post.body}</p>
         `
-        postContainer.appendChild(div)
+        postContainer.appendChild(div)// container er moddhe div ta pathiye dilam
     }
    
+}
+
+
+//add post
+function addPost(){
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+          title: 'foo',
+          body: 'bar',
+          userId: 1,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+    .then(res => res.json())
+    .then((data) => console.log(data));
 }
