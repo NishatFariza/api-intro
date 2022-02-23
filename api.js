f6d99b358498c3600c507c8d96708b18b778ad66
NishatFariza <nishatfarzia78@gmail.com>
2022-02-23 17:41:21 +0600

    dynamic user data

--- api.js
+++ api.js
@@ -11,7 +11,14 @@ function loadUsers(){
 }
 
 function displayUsers(data){
-  console.log({data})
+    console.log(data)
+    const ul =document.getElementById('users');
+  for(const user of data){
+      console.log(user.name);
+      const li =document.createElement('li');
+      li.innerText = `Name: ${user.name} Email: ${user.email}`;
+      ul.appendChild(li);
+  }
 }
 
 //post data
