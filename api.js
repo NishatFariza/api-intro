ba6d35cf6ce1d80ee5f707d99ab7ed1b6a8124fa
NishatFariza <nishatfarzia78@gmail.com>
2022-02-23 16:53:26 +0600

    check github

---
+++ api.js
@@ -0,0 +1,30 @@
+function loadData (){
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json))
+}
+//user data
+function loadUsers(){
+   fetch('https://jsonplaceholder.typicode.com/users')
+   .then(response =>response.json())
+   .then(data =>displayUsers(data))
+}
+
+function displayUsers(data){
+  console.log({data})
+}
+
+//post data
+function loadPost(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(data =>console.log(data))
+}
+// loadPost()
+
+//comment data
+document.getElementById('load-comment').addEventListener('click', function(){
+    fetch('https://jsonplaceholder.typicode.com/comments')
+    .then(res => res.json())
+    .then(data => console.log(data))
+})
